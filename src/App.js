fe4c21c98b244d2591d06ad690e52e829f4a9f0d
dominggo1999 <arnolddominggo19@gmail.com>
2021-08-27 14:55:27 +0700

    client font

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
-import { useRef } from 'react';
+// https://stackoverflow.com/questions/61642758/dynamically-load-font-from-file-based-on-userinput
+
+import { useState, useRef } from 'react';
 
 const App = () => {
   const ref = useRef();
+  const [fontList, setFontList] = useState([]);
+  const [activeFont, setActiveFont] = useState('sans-serif');
 
-  const createFontFace = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const file = ref.current.files[0];
-    const filename = file.name;
-    const fontFamily = filename.split('.')[0];
+    const data = await file.arrayBuffer();
+    const fontFamily = file.name.split('.')[0];
 
-    const myFont = new Font(fontFamily);
+    const font = new FontFace(fontFamily, data);
+    await font.load();
+    document.fonts.add(font);
 
-    // Use filereader to read the file
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-
-    reader.onload = () => {
-      // Pass the buffer, and the original filename
-      myFont.fromDataBuffer(reader.result, file.name);
-      myFont.onload = (e) => {
-        // ...
-      };
-    };
+    // Add font to list
+    setFontList([
+      ...fontList,
+      fontFamily,
+    ]);
   };
 
   return (
     <div>
-      <h1>Upload your font</h1>
-      <form onSubmit={createFontFace}>
+      <form onSubmit={handleSubmit}>
         <input
-          ref={ref}
           type="file"
+          ref={ref}
         />
-        <button type="submit">Upload</button>
+        <button
+          className="bg-red-500 text-white p-3"
+          type="submit"
+        >Upload
+        </button>
       </form>
+
+      <ul className="mb-20">
+        {
+          fontList && fontList.map((font) => {
+            return (
+              <li
+                onClick={() => setActiveFont(font)}
+                key={`font-${font}`}
+              >{font}
+              </li>
+            );
+          })
+        }
+      </ul>
+      <h1
+        className="text-8xl"
+        style={{
+          fontFamily: activeFont,
+        }}
+      >A very big text
+      </h1>
     </div>
   );
 };
