76553966477fc51902ed80b51b5d9f6ed43ae177
dominggo1999 <arnolddominggo19@gmail.com>
2021-08-29 18:21:53 +0700

    bagus

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import { useRef, useState } from 'react';
 import tw, { styled } from 'twin.macro';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import { Rnd } from 'react-rnd';
+
+const Canvas = styled.div`
+  ${tw`
+    mx-auto
+  `}
+  width: 300px;
+  height: 300px;
+  transform: ${(props) => {
+    return `scale(${props.scale})`;
+  }}
+`;
+
+const FixedCanvas = styled.div`
+${tw`
+    bg-red-500 
+  `}
+  width: 300px;
+  height: 300px;
+  margin: 0 auto;
+`;
 
 const Container = styled.div`
   ${tw`
@@ -10,11 +31,11 @@ const Container = styled.div`
     justify-center
     items-center
     bg-red-500
-    overflow-hidden
   `}
 `;
 
 const App = () => {
+  const [scale, setScale] = useState(0.3);
   return (
     <div>
       <Container>
@@ -22,12 +43,21 @@ const App = () => {
           doubleClick={{ disabled: true }}
           minScale={0.1}
           maxScale={2}
-          initialScale={0.3}
+          initialScale={scale}
           limitToBounds={false}
           centerOnInit
           alignmentAnimation={{
             sizeX: 100,
             sizeY: 100,
+          }}
+          wheel={{
+            activationKeys: ['z'],
+          }}
+          panning={{
+            activationKeys: ['x'],
+          }}
+          onZoom={(e) => {
+            setScale(e.state.scale);
           }}
         >
           <TransformComponent>
@@ -39,7 +69,17 @@ const App = () => {
                 }}
                 className="bg-blue-900"
               >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, impedit?
+                <Rnd
+                  default={{
+                    x: 0,
+                    y: 0,
+                    width: 520,
+                    height: 500,
+                  }}
+                  scale={scale}
+                >
+                  <h1 className="text-8xl">geawgewagew</h1>
+                </Rnd>
               </div>
             </div>
           </TransformComponent>
