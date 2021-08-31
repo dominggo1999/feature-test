4ed06ceb281cc6d1523725585898b278fce54a2b
dominggo1999 <arnolddominggo19@gmail.com>
2021-09-01 00:09:15 +0700

    upload font

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ const App = () => {
     'Besley',
     'Lato',
     'Montserrat',
+    'Pacifico',
   ]);
+  const [uploadedFontList, setUploadedFontList] = useState([]);
   const [loading, setLoading] = useState(false);
   const [activeFont, setActiveFont] = useState('sans-serif');
   const [loadedFont, setLoadedFont] = useState([]);
+  const ref = useRef();
 
   const loadFont = (fontFamily) => {
     setLoading(true);
@@ -48,8 +51,33 @@ const App = () => {
     }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const fileInput = ref.current.files;
+
+    const file = fileInput[0];
+    const data = await file.arrayBuffer();
+    const fontFamily = file.name.split('.')[0];
+    if(fileInput.length && uploadedFontList.indexOf(fontFamily) === -1) {
+      const font = new FontFace(fontFamily, data);
+      await font.load();
+      document.fonts.add(font);
+
+      // Add font to list
+      setUploadedFontList([
+        ...uploadedFontList,
+        fontFamily,
+      ]);
+    }else{
+      console.log('font is already exists');
+    }
+    ref.current.value = '';
+  };
+
   return (
-    <div className="bg-blue-400 w-full h-screen">
+    <div className="bg-blue-400 w-full p-10">
+      <h1>Google font</h1>
       <ul className="w-full flex flex-col justify-center">
         {
           fontList.map((f) => {
@@ -64,6 +92,33 @@ const App = () => {
         }
       </ul>
 
+      <h1>Uploaded Font</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          ref={ref}
+          required
+        />
+        <button
+          className="bg-red-500 text-white p-3"
+          type="submit"
+        >Upload
+        </button>
+      </form>
+      <ul className="mb-20 w-full flex flex-col justify-center">
+        {
+          uploadedFontList && uploadedFontList.map((font) => {
+            return (
+              <button
+                onClick={() => setActiveFont(font)}
+                key={`font-${font}`}
+              >{font}
+              </button>
+            );
+          })
+        }
+      </ul>
+
       {/* Content */}
       {loading ? <h1>Loading</h1> : <h1>Ready</h1> }
       <h1
@@ -71,7 +126,7 @@ const App = () => {
           fontFamily: activeFont,
         }}
         className="text-5xl mt-10"
-      >A very big text
+      >A very big text Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident quam sint minima maiores. Porro dolores nihil, voluptatum numquam debitis autem?
       </h1>
     </div>
   );
